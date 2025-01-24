313d1aa97f47af2d50c20b24f746cf911cceef56
Ezequiel <ezequielezequiel9@gmail.com>
2025-01-24 18:41:44 -0300

    edit index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,16 +1,20 @@
-import express, { Request, Response } from "express";
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import express from "express";
 
 const app = express();
 
 // Middleware
 app.use(express.json());
 
-// Endpoints
-app.get("/hello", (req: Request, res: Response) => {
-  console.log("Hola soy el endpoint GET /api");
+// Definir la lógica del endpoint directamente en la función exportada
+export default function (req: VercelRequest, res: VercelResponse) {
+  if (req.method === "GET" && req.url === "/api/hello") {
+    console.log("Hola soy el endpoint GET /api");
+    return res.json({
+      message: "¡Hola desde Express con TypeScript en Vercel!",
+    });
+  }
 
-  res.json({ message: "¡Hola desde Express con TypeScript en Vercel!" });
-});
-
-// Exporta el handler para que funcione en Vercel
-export default app;
+  // Si no coincide con el endpoint, retorna un 404
+  return res.status(404).send("Not Found");
+}
