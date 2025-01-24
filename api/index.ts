22649d6056f9796af640bfdb27b3001e497c3f54
Ezequiel <ezequielezequiel9@gmail.com>
2025-01-24 17:37:12 -0300

    first commit

---
+++ api/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Endpoints
+app.get("/api", (req: Request, res: Response) => {
+  console.log("Hola soy el endpoint GET /api");
+
+  res.json({ message: "¡Hola desde Express con TypeScript en Vercel!" });
+});
+
+// Exporta el handler para que funcione en Vercel
+export default app;
